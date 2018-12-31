63b454c59c44a1d52ab5e280f0c20af458c1c87a
EmeraldNZ <ahyuancy@gmail.com>
2018-12-31 22:30:00 +1300

    Update .js

--- .js
+++ .js
@@ -1,23 +1,20 @@
 function convertHTML(str) {
      let arr = str.split('');
-      for(let i = 0; i < arr.length; i++){    
+     for(let i = 0; i < arr.length; i++){    
           if(arr[i] == '&'){
-          str = str.replace(arr[i],"&​amp;");        
+               str = str.replace(arr[i],"&​amp;");        
           }
           if(arr[i] == '<'){
-          str = str.replace(arr[i],'&​lt;');         
+               str = str.replace(arr[i],'&​lt;');         
           }
-         if(arr[i] == '>'){
-            str = str.replace(arr[i],'&​gt;');
-           
+          if(arr[i] == '>'){
+               str = str.replace(arr[i],'&​gt;');         
           }
-         if(arr[i] == '"'){
-            str = str.replace(arr[i],'&​quot;');
-            
+          if(arr[i] == '"'){
+               str = str.replace(arr[i],'&​quot;');          
           }
           if(arr[i] == "'"){
-            str = str.replace(arr[i],'&apos;');
-          
+               str = str.replace(arr[i],'&apos;');        
           }
       }
       return str;    
