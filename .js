392fdda70c2c16fb2f18f3ecee63d6f1516ad361
EmeraldNZ <ahyuancy@gmail.com>
2018-12-31 22:59:33 +1300

    Update .js

--- .js
+++ .js
@@ -1,3 +1,6 @@
+
+/***********  Solution #1    **************************************************************************************************/
+
 function convertHTML(str) {
      let arr = str.split('');
      for(let i = 0; i < arr.length; i++){    
@@ -22,11 +25,28 @@ function convertHTML(str) {
 convertHTML("Schindler's List"); // return "Schindler&​apos;s List"
 
 
+
 /***********  Solution #2    **************************************************************************************************/
 
 // Chaining of replace method with different arguments
 function convertHTML2(str) {
     return str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");                  
+}
+convertHTM2L("Hamburgers < Pizza < Tacos"); // return "Hamburgers &​lt; Pizza &​lt; Tacos"
+
+
+
+/***********  Solution #3    **************************************************************************************************/
+
+function convertHTML(str){
+    const htmlEntities = {
+        '&':'&amp;',
+        '<':'&lt;',
+        '>':'&gt;',
+        '"':'&quot;',
+        "'":'&apos;'
+    }
+    return str.split('').map(element => htmlEntities[element] || element).join('');
 }
 
 
